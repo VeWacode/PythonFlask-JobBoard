5e56964f30e4ee06dbe3f556b07826ba36e016b2
Vegar Wang <vegarw@microsoft.com>
2020-02-07 14:00:38 +0100

    Completed module

--- jobs/app.py
+++ jobs/app.py
@@ -3,6 +3,6 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/topauthors')
+@app.route('/jobs')
 def TopAuthors():
     return render_template('index.hmtl')
